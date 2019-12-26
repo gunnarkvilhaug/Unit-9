a49d67f9d5a5c0a44b55cbc75aab8faf45e92de5
Gunnar Kvilhaug <gunnar.kvilhaug@appex.no>
2019-12-26 23:23:42 +0100

    Full commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// Heading animation  //////////////////////////////////////////////////////////////////////////////
+
+anime.timeline()
+  .add({
+    targets: '.header-text',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 1250,
+    delay: (el, i) => 250 * (i+1)
+  })
+
+
+// Header/navbar ////////////////////////////////////////////////////////////////////////////////////
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+/// Smooth scroll //////////////////////////////////////////////////////////////////////////////////
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
